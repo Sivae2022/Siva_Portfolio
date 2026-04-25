03e35707da49d04709edfd583306a005b3ec5771
Siva <siva.e2022cse@sece.ac.in>
2026-04-25 15:56:27 +0530

    update hire me section with resume link fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Siva_Portfolio/',
   server: {
     port: 8001,
   },
